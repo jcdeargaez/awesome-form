4d98d0af96a78cbc023ce460d7cd2c5a44e56df0
Juan Argaez <jc.argaez@gmail.com>
2019-06-03 11:04:02 -0500

    Update settings.py

--- awesome-form/settings.py
+++ awesome-form/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'awesome-form.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
